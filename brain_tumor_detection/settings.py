7e860b10abe26f38ba9172bc266de730b4ac8208
Ayush Savaliya <ayushsavaliya170@gmail.com>
2025-11-30 00:20:23 +0530

    Fixed config

--- brain_tumor_detection/settings.py
+++ brain_tumor_detection/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-!ojf*162h1zph3ey3fjd-)x3myyglsl@i-s(ol9+22=n==va3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
